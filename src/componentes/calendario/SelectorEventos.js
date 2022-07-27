a914a0595747b3553010b75b399944e10e792d71
Fernando Iglesias <fniglesias@outlook.com>
2022-07-27 13:27:54 -0300

    cambio class por classname

--- src/componentes/calendario/SelectorEventos.js
+++ src/componentes/calendario/SelectorEventos.js
@@ -1,30 +1,30 @@
 function SelectorEventos(){
     return (
-        <div class="check_container">
+        <div className="check_container">
 
           <h6>Buscar por:</h6>
           <label>
-            <input type="checkbox" class="filled-in color-cobranzas" checked="checked" id="checkbox_cobranzas" />
+            <input type="checkbox" className="filled-in color-cobranzas" checked="checked" id="checkbox_cobranzas" />
             <span>COBRANZAS</span>
           </label>
 
           <label>
-            <input type="checkbox" class="filled-in color-cupos" checked="checked" id="checkbox_cupos" />
+            <input type="checkbox" className="filled-in color-cupos" checked="checked" id="checkbox_cupos" />
             <span>CUPOS OTORGADOS</span>
           </label>        
 
           <label>
-            <input type="checkbox" class="filled-in color-vencimientos" checked="checked" id="checkbox_vencimientos" />
+            <input type="checkbox" className="filled-in color-vencimientos" checked="checked" id="checkbox_vencimientos" />
             <span>VENCIMIENTOS</span>
           </label>         
 
           <label>
-            <input type="checkbox" class="filled-in color-futuros" checked="checked" id="checkbox_futuros" />
+            <input type="checkbox" className="filled-in color-futuros" checked="checked" id="checkbox_futuros" />
             <span>FECHAS DE FUTUROS</span>
           </label>      
 
           <label>
-            <input type="checkbox" class="filled-in color-todos" checked="checked" id="checkbox_todos" />
+            <input type="checkbox" className="filled-in color-todos" checked="checked" id="checkbox_todos" />
             <span>TODOS</span>
           </label>
 
