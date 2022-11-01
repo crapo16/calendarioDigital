8f77d3568592aea59af29bc660a6cdf1f3588ed0
Nicolás Espinosa <ne_espinosa@hotmail.com>
2022-11-01 14:17:34 -0300

    Ajuste para mostrar eventos del dia, usuario y cuentas

--- src/componentes/eventoshome/EventosHome.js
+++ src/componentes/eventoshome/EventosHome.js
@@ -2,119 +2,122 @@ import './EventosHome.css';
 
 /*import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'*/
-import {useState,useEffect,memo} from 'react';
+import { useState, useEffect, memo } from 'react';
 import CobranzasHome from '../cobranzas/home/CobranzasHome';
 import CuposHome from '../cupos/home/CuposHome';
 import VencimientosHome from '../vencimientos/home/VencimientosHome';
 import FuturosHome from '../futuros/home/FuturosHome';
-import EventosZeniHome from '../eventosZeni/home/EventosZeniHome';
 import { EventosContext } from '../../context/EventosContext';
-
 import { Link, useParams } from 'react-router-dom';
 import { useContext } from 'react';
 
 
+function EventosHome({ nombreUsuario, nroCuenta, nombreCuenta }) {
 
-function EventosHome({nombreUsuario, nroCuenta, nombreCuenta}){
+    const { user, hash, fechaHasta } = useParams();
 
-    const {user,hash,fechaHasta}=useParams();
+    const { setDatosUsuario, eventos } = useContext(EventosContext);
+    const [visibilidadCobranzas, setVisibilidadCobranzas] = useState(false);
+    const [visibilidadEventosZeni, setVisibilidadEventosZeni] = useState(false);
+    const [visibilidadCupos, setVisibilidadCupos] = useState(false);
+    const [visibilidadFuturos, setVisibilidadFuturos] = useState(false);
+    const [visibilidadVencimientos, setVisibilidadVencimientos] = useState(false);
+    const [visibilidadTodos, setVisibilidadTodos] = useState(true);
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    const [fechaHoy] = useState(new Date().toLocaleDateString('es-AR', options).charAt(0).toUpperCase() + new Date().toLocaleDateString('es-AR', options).slice(1));
 
-    const {setDatosUsuario,eventos}=useContext(EventosContext);
-    const [visibilidadCobranzas, setVisibilidadCobranzas]=useState(false);
-    const [visibilidadEventosZeni, setVisibilidadEventosZeni]=useState(false);
-    const [visibilidadCupos, setVisibilidadCupos]=useState(false);
-    const [visibilidadFuturos, setVisibilidadFuturos]=useState(false);
-    const [visibilidadVencimientos, setVisibilidadVencimientos]=useState(false);
-    const [visibilidadTodos, setVisibilidadTodos]=useState(true);
-    
-    function cambiarTodos(visibilidad){
+    function cambiarTodos(visibilidad) {
 
-        if(visibilidad)
-        {
-        setVisibilidadCobranzas(false);
-        setVisibilidadEventosZeni(false);
-        setVisibilidadCupos(false);
-        setVisibilidadFuturos(false);
-        setVisibilidadVencimientos(false);
-        setVisibilidadTodos(true);
+        if (visibilidad) {
+            setVisibilidadCobranzas(false);
+            setVisibilidadEventosZeni(false);
+            setVisibilidadCupos(false);
+            setVisibilidadFuturos(false);
+            setVisibilidadVencimientos(false);
+            setVisibilidadTodos(true);
         }
-        else{
-            
+        else {
+
             setVisibilidadTodos(false);
         }
     }
 
-    function cambiarCobranzas(visibilidad){
+    function cambiarCobranzas(visibilidad) {
         setVisibilidadCobranzas(visibilidad);
         setVisibilidadTodos(false);
     }
 
-    function cambiarCupos(visibilidad){
+    function cambiarCupos(visibilidad) {
         setVisibilidadCupos(visibilidad);
         setVisibilidadTodos(false);
 
     }
 
-    function cambiarFuturos(visibilidad){
+    function cambiarFuturos(visibilidad) {
         setVisibilidadFuturos(visibilidad);
         setVisibilidadTodos(false);
 
     }
 
-    function cambiarVencimientos(visibilidad){
+    function cambiarVencimientos(visibilidad) {
         setVisibilidadVencimientos(visibilidad);
         setVisibilidadTodos(false);
 
     }
 
-    useEffect(()=>{
-        setDatosUsuario(user,hash,fechaHasta);
+    useEffect(() => {
+        setDatosUsuario(user, hash, fechaHasta);
 
-
-
-    },[user,hash,fechaHasta])
+    }, [user, hash, fechaHasta])
     return (
         <div>
             <div className='user-container section'>
-                <h4> Buen día {nombreUsuario}</h4>
-                <h5> Cuenta {nroCuenta}, {nombreCuenta}</h5>
+                {
+
+                    eventos['usuario'] && eventos['usuario'].razonSocial ? <h4> Buen día {eventos['usuario'].razonSocial}</h4> : <h4></h4>
+                }
+
+                {
+                    eventos['usuario'] ? eventos['usuario'].cuentas.map(e =>
+                        <h5>Cuenta {e.numeroCuenta}, {e.denominacionCuenta}</h5>) :
+                        <h5></h5>}
             </div>
             <div className='date-container'>
                 <div className='row'>
-                    <h3>Martes 17 de mayo, 2022</h3>
+                    <h3>{fechaHoy}</h3>
                 </div>
 
                 <div className="botonera" id="botonera">
                     <div>
-                        <div className={visibilidadCobranzas?"chip c-cobranzas active":"chip c-cobranzas"} onClick={()=>cambiarCobranzas(!visibilidadCobranzas)}  >COBRANZAS</div>
-                        <div className={visibilidadCupos?"chip c-cupos active":"chip c-cupos"} onClick={()=>cambiarCupos(!visibilidadCupos)}>CUPOS OTORGADOS</div>
-                        <div className={visibilidadVencimientos?"chip c-vencimientos active":"chip c-vencimientos"} onClick={()=>cambiarVencimientos(!visibilidadVencimientos)}>VENCIMIENTOS</div>
-                        <div className={visibilidadFuturos?"chip c-futuros active":"chip c-futuros"} onClick={()=>cambiarFuturos(!visibilidadFuturos)}>FUTUROS Y OPCIONES</div>
+                        <div className={visibilidadCobranzas ? "chip c-cobranzas active" : "chip c-cobranzas"} onClick={() => cambiarCobranzas(!visibilidadCobranzas)}  >COBRANZAS</div>
+                        <div className={visibilidadCupos ? "chip c-cupos active" : "chip c-cupos"} onClick={() => cambiarCupos(!visibilidadCupos)}>CUPOS OTORGADOS</div>
+                        <div className={visibilidadVencimientos ? "chip c-vencimientos active" : "chip c-vencimientos"} onClick={() => cambiarVencimientos(!visibilidadVencimientos)}>VENCIMIENTOS</div>
+                        <div className={visibilidadFuturos ? "chip c-futuros active" : "chip c-futuros"} onClick={() => cambiarFuturos(!visibilidadFuturos)}>FUTUROS Y OPCIONES</div>
                         {/* <div className={visibilidadEventosZeni?"chip c-eventos active":"chip c-eventos"} onClick={()=>setVisibilidadEventosZeni(!visibilidadEventosZeni)}>EVENTOS</div> */}
-                        <div className={visibilidadTodos?"chip c-todos active active":"chip c-todos"} onClick={()=>cambiarTodos(!visibilidadTodos)}>TODOS</div>
+                        <div className={visibilidadTodos ? "chip c-todos active active" : "chip c-todos"} onClick={() => cambiarTodos(!visibilidadTodos)}>TODOS</div>
                     </div>
                     <div>
-                        <Link to={'/calendario'}>       
-                        <div className="btn waves-effect indigo lighten-5 color-primary">
-                            <i className="material-icons left">today</i>
-                            VER CALENDARIO
-                        </div>
+                        <Link to={'/calendario'}>
+                            <div className="btn waves-effect indigo lighten-5 color-primary">
+                                <i className="material-icons left">today</i>
+                                VER CALENDARIO
+                            </div>
                         </Link>
                     </div>
                 </div>
-                
+
 
                 <div className='event-container'>
-                    <CobranzasHome visibilidadCobranzas={visibilidadCobranzas} visibilidadTodos={visibilidadTodos}/>
-                    <CuposHome visibilidadCupos={visibilidadCupos} visibilidadTodos={visibilidadTodos}/>
-                    <VencimientosHome  visibilidadVencimientos={visibilidadVencimientos} visibilidadTodos={visibilidadTodos}/>
-                    <FuturosHome visibilidadFuturos={visibilidadFuturos} visibilidadTodos={visibilidadTodos}/>
+                    <CobranzasHome visibilidadCobranzas={visibilidadCobranzas} visibilidadTodos={visibilidadTodos} />
+                    <CuposHome visibilidadCupos={visibilidadCupos} visibilidadTodos={visibilidadTodos} />
+                    <VencimientosHome visibilidadVencimientos={visibilidadVencimientos} visibilidadTodos={visibilidadTodos} />
+                    <FuturosHome visibilidadFuturos={visibilidadFuturos} visibilidadTodos={visibilidadTodos} />
                     {/* <EventosZeniHome visibilidadEventosZeni={visibilidadEventosZeni} visibilidadTodos={visibilidadTodos}/> */}
-                    {!visibilidadTodos && !visibilidadVencimientos && !visibilidadCobranzas && !visibilidadCupos && !visibilidadEventosZeni && !visibilidadFuturos?
-                        <p>Seleccione al menos un item para ver los eventos</p>:""
+                    {!visibilidadTodos && !visibilidadVencimientos && !visibilidadCobranzas && !visibilidadCupos && !visibilidadEventosZeni && !visibilidadFuturos ?
+                        <p>Seleccione al menos un item para ver los eventos</p> : ""
                     }
-                 </div>
-                
+                </div>
+
             </div>
         </div>
     )
